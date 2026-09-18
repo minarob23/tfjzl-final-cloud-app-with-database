7eda2c8779cd128d5382cf9e0c765fcac8e6037a
minarob23 <menarobir246rm@gmail.com>
2026-09-18 22:18:21 +0300

    Redirect site root to course index

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='onlinecourse:index', permanent=False)),
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
